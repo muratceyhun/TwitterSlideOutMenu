25184207fd9520f455a17fcdd3564a59d1c81ff6
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-09-11 18:26:50 +0300

    Showing Menu Controller

---
+++ TwitterSlideOutMenu/Controllers/MenuController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuController.swift
+//  TwitterSlideOutMenu
+//
+//  Created by Murat Ceyhun Korpeoglu on 11.09.2023.
+//
+
+import UIKit
+
+
+class MenuController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
--- TwitterSlideOutMenu/ViewController.swift
+++ TwitterSlideOutMenu/ViewController.swift
@@ -16,10 +16,12 @@ class ViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
         view.backgroundColor = .red
         setupNavigationItems()
-//        tableView.separatorStyle = .none
     }
     
     fileprivate func setupNavigationItems() {
+        
+        navigationItem.title = "Home"
+//        tableView.separatorStyle = .none
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
     }
@@ -30,6 +32,26 @@ class ViewController: UITableViewController {
     }
     
     @objc fileprivate func handleOpen() {
+        
+        let vc = MenuController()
+        
+        vc.view.frame = CGRect(x: 0, y: 0, width: 300, height: view.frame.height)
+        
+        let keyWindow = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .compactMap({$0 as? UIWindowScene})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
+        
+        
+        keyWindow?.addSubview(vc.view)
+                
+        
+//        view.addSubview(vc.view)
+        
+        
+        
+        
         print("Open button pressed.")
     }
     
@@ -40,6 +62,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+//        let cell2 = UITableViewCell(style: .default, reuseIdentifier: "cellID") You don't need to register for prototype cells.
         cell.textLabel?.text = "\(indexPath.row)"
         return cell
     }
