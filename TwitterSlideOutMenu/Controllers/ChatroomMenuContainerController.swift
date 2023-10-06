3e6c3d29441d8084a9c1573327d865c0ba09bac2
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-10-06 22:35:45 +0300

    Chatroom Filter Delegate

--- TwitterSlideOutMenu/Controllers/ChatroomMenuContainerController.swift
+++ TwitterSlideOutMenu/Controllers/ChatroomMenuContainerController.swift
@@ -12,8 +12,8 @@ class ChatroomMenuContainerController: UIViewController {
     
     
     let chatroomsMenuController = ChatroomsMenuController()
-    let searchContainer = UIView()
-    let rocketImageView = UIImageView(image: #imageLiteral(resourceName: "rocket"))
+    let searchContainer = SearchContainerView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,27 +21,20 @@ class ChatroomMenuContainerController: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.3074549437, green: 0.2199839652, blue: 0.2903968394, alpha: 1)
         let chatroomsView = chatroomsMenuController.view!
         view.addSubview(chatroomsView)
-//        chatroomsView.fillSuperview()
         view.addSubview(searchContainer)
+        searchContainer.searchBar.delegate = chatroomsMenuController
         searchContainer.backgroundColor = #colorLiteral(red: 0.249730736, green: 0.1816008389, blue: 0.2514018416, alpha: 1)
-        rocketImageView.contentMode = .scaleAspectFit
-        rocketImageView.layer.cornerRadius = 4
-        rocketImageView.clipsToBounds = true
-        view.addSubview(rocketImageView)
-        rocketImageView.anchor(top: nil, leading: view.leadingAnchor, bottom: searchContainer.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 8, bottom: 8, right: 0), size: .init(width: 40, height: 40))
+    
+    
         
         
         
         // WARNING !!!!
-        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).textColor = .white
-        
-        
-        
         
         
         let searchBar = UISearchBar()
         searchContainer.addSubview(searchBar)
-        searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: searchContainer.bottomAnchor, trailing: view.trailingAnchor, padding: .zero)
+        
         searchBar.searchBarStyle = .minimal
         searchBar.placeholder = "dsadasad"
 
@@ -50,4 +43,50 @@ class ChatroomMenuContainerController: UIViewController {
         chatroomsView.anchor(top: searchContainer.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
         
     }
+    
+    
+    
+    class SearchContainerView: UIView {
+        
+        let searchBar: UISearchBar = {
+            let sb = UISearchBar()
+            sb.searchBarStyle = .minimal
+            sb.placeholder = "Enter for filtering..."
+            return sb
+        }()
+        
+        
+        let rocketImageView: UIImageView = {
+           let image = UIImageView(image: UIImage(named: "rocket"))
+            image.contentMode = .scaleAspectFit
+            image.layer.cornerRadius = 8
+            image.clipsToBounds = true
+            return image
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).textColor = .white
+            addSubview(searchBar)
+            addSubview(rocketImageView)
+            rocketImageView.anchor(top: nil, leading: safeAreaLayoutGuide.leadingAnchor, bottom: bottomAnchor, trailing: nil, padding: .init(top: 0, left: 8, bottom: 10, right: 0), size: .init(width: 40, height: 40))
+            searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 2, right: 0))
+
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- TwitterSlideOutMenu/Controllers/ChatroomsMenuController.swift
+++ TwitterSlideOutMenu/Controllers/ChatroomsMenuController.swift
@@ -7,7 +7,40 @@
 
 import UIKit
 
+extension ChatroomsMenuController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        var results = [[String]]()
+        
+        
+        if searchText.isEmpty {
+            filteredGroups = chatroomsGroups
+            tableView.reloadData()
+            return
+        }
+        
+//        chatroomsGroups.forEach { group in
+//            let filteredGroup = group.filter { chatroomName in
+//                return chatroomName.lowercased().contains(searchText.lowercased())
+//            }
+//
+//            results.append(filteredGroup)
+//        }
+//        print(results)
+//        filteredGroups = results
+        
+        filteredGroups = chatroomsGroups.map({ group -> [String] in
+            return group.filter {$0.lowercased().contains(searchText.lowercased())}
+        })
+        
+        tableView.reloadData()
+
+    }
+    
+}
+
 class ChatroomsMenuController: UITableViewController {
+    
     
     
     let chatroomsGroups =
@@ -17,12 +50,18 @@ class ChatroomsMenuController: UITableViewController {
         ["Joe Biden", "Tim Cook", "Steve Jobs", "Murat Ceyhun Korpeoglu"]
     ]
     
+    var filteredGroups = [[String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        tableView.backgroundColor = .orange
         tableView.separatorStyle = .none
         tableView.backgroundColor = #colorLiteral(red: 0.3074549437, green: 0.2199839652, blue: 0.2903968394, alpha: 1)
+        
+        
+        // This is making a copy, not a direct reference....
+        filteredGroups = chatroomsGroups
         
     }
     
@@ -55,16 +94,16 @@ class ChatroomsMenuController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return chatroomsGroups.count
+        return filteredGroups.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatroomsGroups[section].count
+        return filteredGroups[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = ChatroomMenuCell(style: .default, reuseIdentifier: nil)
-        let text = chatroomsGroups[indexPath.section][indexPath.row]
+        let text = filteredGroups[indexPath.section][indexPath.row]
         cell.backgroundColor = .clear
         cell.textLabel?.textColor = .white
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
